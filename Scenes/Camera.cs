fa20a9800bd244e87e9f5a671d1767e96ca6b3fe
VioletGminisJ25 <javierrodal25@gmail.com>
2024-08-29 17:41:44 +0200

    adding external editor, animation player, camera

--- Scenes/Camera.cs
+++ Scenes/Camera.cs
@@ -1,15 +1,19 @@
 using Godot;
+using Godot.NativeInterop;
 using System;
 
 public partial class Camera : Camera2D
 {
-	// Called when the node enters the scene tree for the first time.
+	
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		CharacterBody2D player = GetTree().GetNodesInGroup("Player")[0] as CharacterBody2D;
+		Vector2 position =  GlobalPosition;
+		position = player.GlobalPosition;
+		GlobalPosition = position;
 	}
 }
--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -3,38 +3,71 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 200.0f;
+	public const float JumpVelocity = -300.0f;
+	private static AnimatedSprite2D animationPlayer;
+	private static CharacterBody2D player;
+	private Vector2 velocity;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		// Vector2 position = Position;
+		animationPlayer = GetTree().GetNodesInGroup("Player")[0].GetChild(1) as AnimatedSprite2D;
+		velocity = Velocity;
+		Animations();
 
-		// Add the gravity.
 		if (!IsOnFloor())
 		{
+			if (velocity.Y > 0)
+			{
+				animationPlayer.Play("fall");
+			}
 			velocity += GetGravity() * (float)delta;
+
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_space") && IsOnFloor())
 		{
+			animationPlayer.Play("jump");
 			velocity.Y = JumpVelocity;
 		}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
+
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
+			if (velocity.X > 0)
+			{
+				animationPlayer.FlipH = false;
+			}
+			else
+			{
+				animationPlayer.FlipH = true;
+			}
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+	private void Animations()
+	{
+
+		if (velocity.X != 0)
+		{
+			animationPlayer.Play("run");
+		}
+		else
+		{
+			animationPlayer.Play("idle");
+		}
+	}
+
+
+
 }
