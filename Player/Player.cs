c9daa63792d197e11053ccd63d67ff8fb7bbc8e3
VioletGminisJ25 <javierrodal25@gmail.com>
2024-09-27 18:06:59 +0200

    refactoring, player movment, new sprite

--- Player/Player.cs
+++ Player/Player.cs
@@ -4,104 +4,121 @@ using System.Numerics;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 170.0f;
-	public const float JumpVelocity = -300.0f;
-	private static AnimatedSprite2D animationPlayer;
-	private Godot.Vector2 velocity;
-	private Godot.Vector2 particlesScale;
-	private Godot.Vector2 particlesPosition;
-	private static CpuParticles2D particles;
+    public const float Speed = 170.0f;
+    public const float JumpVelocity = -300.0f;
+    private static AnimatedSprite2D animationPlayer;
+    private Godot.Vector2 velocity;
+    private Godot.Vector2 particlesScale;
+    private Godot.Vector2 particlesPosition;
+    private static CpuParticles2D particles;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		// Vector2 position = Position;
-		animationPlayer = GetTree().GetNodesInGroup("Player")[0].GetChild(1) as AnimatedSprite2D;
-		particles = GetTree().GetNodesInGroup("Player")[0].GetChild(2) as CpuParticles2D;
-		particlesScale = particles.Scale;
-		particlesPosition = particles.Position;
-		particles.Emitting = false;
-		velocity = Velocity;
-		Animations();
+    public override void _PhysicsProcess(double delta)
+    {
+        // Vector2 position = Position;
+        animationPlayer = GetTree().GetNodesInGroup("Player")[0].GetChild(1) as AnimatedSprite2D;
+        particles = GetTree().GetNodesInGroup("Player")[0].GetChild(2) as CpuParticles2D;
+        particlesScale = particles.Scale;
+        particlesPosition = particles.Position;
+        particles.Emitting = false;
+        velocity = Velocity;
 
-		if (!IsOnFloor())
-		{
-			if (velocity.Y > 0)
-			{
-				animationPlayer.Play("fall");
-			}
-			velocity += GetGravity() * (float)delta;
+       //Vertical movement
+        if (!IsOnFloor())
+        {
 
-		}
+            velocity += GetGravity() * (float)delta;
 
-		if (Input.IsActionJustPressed("ui_space") && IsOnFloor())
-		{
-			animationPlayer.Play("jump");
-			velocity.Y = JumpVelocity;
-		}
+        }
+        else
+        {
+            Particles();
+            if (Input.IsActionJustPressed("ui_space"))
+            {
+                velocity.Y = JumpVelocity;
 
+            }
+        }
 
-		Godot.Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Godot.Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			if (velocity.X > 0)
-			{
-				animationPlayer.FlipH = false;
-			}
-			else
-			{
-				animationPlayer.FlipH = true;
-			}
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        //Horizontal movement
+        Godot.Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        if (direction != Godot.Vector2.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            //Flip sprite direction
+            if (velocity.X > 0)
+            {
+                animationPlayer.FlipH = false;
+            }
+            else
+            {
+                animationPlayer.FlipH = true;
+            }
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 
-		}
+        }
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
-	private void Animations()
-	{
+        Velocity = velocity; //Actualizar velocidad mediante una variable no a pelo??
+        MoveAndSlide();
+        Animations();
+    }
+    private void Animations()
+    {
+        if (IsOnFloor())
+        {
 
-		if (velocity.X != 0)
-		{
-			animationPlayer.Play("run");
-			
-			if (IsOnFloor())
-
-			{
-				particles.Emitting = true;
-
-				if (velocity.X > 0)
-				{
-					particlesScale.X = (float)0.6;
-					particlesPosition.X = -4;
-					particles.Scale = particlesScale;
-					particles.Position = particlesPosition;
-				}
-				else
-				{
-					particlesScale.X = (float)-0.6;
-					particlesPosition.X = 4;
-					particles.Scale = particlesScale;
-					particles.Position = particlesPosition;
-				}
-			}
-		}
-		else
-		{
-			animationPlayer.Play("idle");
-		}
-	}
-
-	public static void _on_cpu_particles_2d_finished()
-	{
-		particles.Emitting = false;
-	}
+            if (velocity.X != 0)
+            {
+                animationPlayer.Play("run");
 
 
+            }
+            else
+            {
+                animationPlayer.Play("idle");
+            }
+        }
+        else
+        {
 
+            if (velocity.Y > 0)
+            {
+                animationPlayer.Play("fall");
+            }
+            else
+            {
+                animationPlayer.Play("jump");
+            }
+        }
+    }
+    public void Particles()
+    {
+        particles.Emitting = true;
 
+        if (velocity.X > 0)
+        {
+            particlesScale.X = (float)0.6;
+            particlesPosition.X = -4;
+            particles.Scale = particlesScale;
+            particles.Position = particlesPosition;
+        }
+        else
+        {
+            particlesScale.X = (float)-0.6;
+            particlesPosition.X = 4;
+            particles.Scale = particlesScale;
+            particles.Position = particlesPosition;
+        }
+    }
+    public static void _on_cpu_particles_2d_finished()
+    {
+        particles.Emitting = false;
+    }
 }
+
+
+
+
+
